03a22432792bd0474aea396d28fde6abbc9b2857
Daniyar <myrzaakim.nurlanbekov@gmail.com>
2023-04-28 15:29:48 +0600

    modified css and busket

--- src/components/Busket/Busket.jsx
+++ src/components/Busket/Busket.jsx
@@ -1,76 +1,119 @@
-
-
-import tick from "../../assets/tick circle_.png"
-
-
-
-
-
-
-import React, { useContext, useState } from 'react';
+import tick from "../../assets/tick circle_.png";
+import emptyBusket from "../../assets/empty-busket.gif";
+import React, { useContext, useState, useEffect } from 'react';
 import { ContextBox } from '../../App';
-import "./Busket.css"
+import "./Busket.css";
 
-const Busket = (props) => {
-  const [box, setBox] = useContext(ContextBox)
-  const [totalItems, setTotalItems] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0);
+const Busket = () => {
+    const [box, setBox] = useContext(ContextBox);
+    const [totalItems, setTotalItems] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0);
 
-  const allProducts = box.map((item, index) => {
+    useEffect(() => {
+        let itemsCount = 0;
+        let totalPrice = 0;
+
+        for (let i = 0; i < box.length; i++) {
+            const count = box[i].count || 1;
+            itemsCount += count;
+            totalPrice += Number(box[i].price) * count;
+        }
+
+        setTotalItems(itemsCount);
+        setTotalPrice(totalPrice);
+    }, [box]);
+
+    const handleIncrease = (index) => {
+        const updatedBox = [...box];
+        updatedBox[index].count = (updatedBox[index].count || 1) + 1;
+        setBox(updatedBox);
+    };
+
+    const handleDecrease = (index) => {
+        const updatedBox = [...box];
+
+        if (updatedBox[index].count > 1) {
+            updatedBox[index].count -= 1;
+            setBox(updatedBox);
+        }
+    };
+
+    const handleRemove = (index) => {
+        const updatedBox = [...box];
+        updatedBox.splice(index, 1)
+        setBox(updatedBox)
+
+        updatedBox.length == 0 ? localStorage.removeItem("box") : []
+    };
+
+    const handleClear = () => {
+        setBox([]);
+        localStorage.removeItem("box");
+    };
+
+    const allProducts = box.map((item, index) => (
+        <div className="bucketItem" key={index}>
+            <div className="tick">
+                <button onClick={() => handleRemove(index)}>
+                    <img src={tick} alt="" />
+                </button>
+            </div>
+            <div className="bucketPhoto">
+                <img className='busket-img' src={item.img} alt="" />
+            </div>
+            <div className="aboutProduct">
+                <div className="productName">
+                    {item.name}
+                </div>
+                <div className="productInfo">
+                    <div className="productPrice">
+                        {item.price} сом
+                    </div>
+                    <div className="productCounter">
+                        <button className='decrease' onClick={() => handleDecrease(index)}> - </button>
+                        <input className='count' type="text" value={item.count || 1} readOnly />
+                        <button className='increase' onClick={() => handleIncrease(index)}> + </button>
+                    </div>
+                    <div className="productPrice">
+                        {item.price * (item.count || 1)} сом
+                    </div>
+                </div>
+            </div>
+            <div className="line"></div>
+        </div>
+    ));
+
     return (
-      <div className="bucketItem" key={index}>
-        <div className="tick">
-          <button>
-            <img src={tick} alt="" />
-          </button>
-        </div>
-        <div className="bucketPhoto">
-          <img className='busket-img' src={item.img} alt="" />
-        </div>
-        <div className="aboutProduct">
-          <div className="productName">
-            {item.name}
-          </div>
-          <div className="productInfo">
-            <div className="productPrice">
-              {item.priсe} сом
+        <div>
+            {box?.length > 0 ? (
+               <div className="cart-info">
+                <h1 className="total-item">Total items: {totalItems}</h1>
+                <h1 className="total-price">Total price: {totalPrice} сом</h1>
+                { allProducts }</div> 
+            ) : ( <div className="colorchik">
+            <div className="cart-info">
+                <h1 className="total-item">Total items: {totalItems}</h1>
+                <h1 className="total-price">Total price: {totalPrice} сом</h1>
             </div>
-            <div className="productCounter">
-              <button className='decrease'>-</button>
-              <input className='count' type="text" value={1} />
-              <button className='increase'>+</button>
+            <div className="text-center">
+                <h3 style={{paddingTop: "50px"}}>The busket is empty 👀 </h3>
+                <p>
+                    Most likely, you have not chosen anything. To order, go to the Catalog page.
+                </p>
+                <img src={emptyBusket} alt="" />
+                <br/>
             </div>
-            <div className="productPrice">
-              {item.price}
-            </div>
-          </div>
+            
+<div className="clear-button">
+<button className="clear-all" onClick={handleClear}>Clear all Cart</button>
+
+</div>
+ </div>
+            )}
+
         </div>
-      </div>
-    )
-
-  })
-
-  React.useEffect(() => {
-    let itemsCount = 0;
-    let totalPrice = 0;
-    for (let i = 0; i < box.length; i++) {
-      itemsCount += 1;
-      console.log(box[i].price)
-      totalPrice += Number(box[i].price);
-    }
-    setTotalItems(itemsCount);
-    setTotalPrice(totalPrice);
-  }, [box]);
-
-  return (
-    <>
-      <div className="cart-info">
-        <p>Total items: {totalItems}</p>
-        <p>Total price: {totalPrice} сом</p>
-      </div>
-      {allProducts}
-    </>
-  );
+    );
 };
 
 export default Busket;
+
--- src/components/catalog/Catalog0.jsx
+++ src/components/catalog/Catalog0.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import "./Catalog0.css"
 
-import {useState, useEffect} from "react"
+
+import { useState, useEffect } from "react"
 
 import { collection, query, getDocs } from "firebase/firestore";
 import { database } from "../../firebase/Firebase";
 import { Link } from 'react-router-dom';
-
+import "./Catalog0.css"
 
 
 
@@ -26,8 +26,8 @@ const Catalog0 = () => {
         const querySnapshot = await getDocs(q);
         let category = []
         querySnapshot.forEach((doc) => {
-           category.push({...doc.data(), id: doc.id})
-           console.log(doc.id)
+            category.push({ ...doc.data(), id: doc.id })
+            console.log(doc.id)
         });
         setCategories(category)
     }
@@ -35,112 +35,89 @@ const Catalog0 = () => {
     const showAllCategory = categories.map((category, index) => {
         return (
             <Link to={`/category/${category.id}`}>
-                <div>
-                   
-                     <div className="catalogContainer" key={index}>
-                         <div classxName="catalogItem"><img className="catalogItem" src={category.img} alt="" />
-                             <div className="itemBlur">
-                                <div className="itemTitle">
-                                 {category.name}
-                                </div>
+
+                    <div className="catalogItem">
+                        <img className="catalogItemImg" src={category.img} alt="" />
+                        <div className="itemBlur">
+                            <div className="itemTitle">
+                                {category.name}
                             </div>
-                         </div>
-                      </div>
+                        </div>
                     </div>
-                    {/* <img src={category.img} alt="" /> */}
-    
+                {/* <img src={category.img} alt="" /> */}
             </Link>
-            
         )
     })
-    
+
     return (
         <div>
-           <div className='container2'>
-              <div>
-               {showAllCategory}
-              </div>
-           </div>
-           
-            
+            <div className='container2'>
+                <div className='catalogContainer'>
+                    {showAllCategory}
+                </div>
+            </div>
+
+
         </div>
     )
 };
 
 
-    // return (
-    //     <>
-    //     <div>1</div>
-        
-
-           
-    //         <section>
-    //             <div className='container2'>
-    //                 <div className="catalogContainer">
-    //                     <a href='/product'><div className="catalogItem">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Струнные
-    //                             </div>
-    //                         </div>
-    //                     </div></a>
-    //                     <div className="catalogItem">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Клавишные
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                     <div className="catalogItem">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Ударные
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                     <div className="catalogItem">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Смычковые
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                     <div className="catalogItem">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Духовые
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                     <div className="catalogItem">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Мандолины
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                     <div className="catalogItem itemblu">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Чехлы
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                     <div className="catalogItem itemblu">
-    //                         <div className="itemBlur">
-    //                             <div className="itemTitle">
-    //                                 Аксессуары
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </section>
-          
-    //     </>
+// return (
+//     <>
+//     <div>1</div>
 
 
-    // );
 
-
-export default Catalog0;
+//         <section>
+//             <div className='container2'>
+//                 <div className="catalogContainer">
+//                     <a href='/product'><div className="catalogItem">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Струнные
+//                             </div>
+//                         </div>
+//                     </div></a>
+//                     <div className="catalogItem">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Клавишные
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="catalogItem">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Ударные
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="catalogItem">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Смычковые
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="catalogItem">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Духовые
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="catalogItem">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Мандолины
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="catalogItem itemblu">
+//                         <div className="itemBlur">
+//                             <div className="itemTitle">
+//                                 Чехлы
+//                             </div>
+//
+export default Catalog0
